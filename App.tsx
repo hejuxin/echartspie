b6e5f5bf75e1d9451cea320c8694fcb8576dfc86
hejuxin <42193700+hejuxin@users.noreply.github.com>
2023-01-10 16:31:47 +0800

    添加utils

--- App.tsx
+++ App.tsx
@@ -1,11 +1,73 @@
 import * as React from 'react';
 import './style.css';
+import {
+  // TooltipDemo1,
+  // TooltipDemo2,
+  // LabelDemo1,
+  // LabelDemo2,
+  // LabelDemo3,
+  // // LabelDemo4,
+  // LegendDemo1,
+  // LegendDemo2,
+  // LegendDemo3,
+  // LegendDemo4,
+  // DoughnutDemo1,
+  // DoughnutDemo2,
+  // DoubleDemo1,
+  // DoubleDemo2,
+  // DoubleDemo3,
+  // AutoDemo1,
+  // AutoDemo2,
+  // LabelLegendDemo1,
+  // LabelLegendDemo2,
+
+  DoubleDemo3,
+} from './Demo';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <React.Fragment>
+      {/* <div>
+        <h2>tooltip自定义</h2>
+        <TooltipDemo1 />
+        <TooltipDemo2 />
+      </div> */}
+      {/* <div>
+        <h2>图例自定义</h2>
+        <LegendDemo1 />
+        <LegendDemo2 />
+        <LegendDemo3 />
+        <LegendDemo4 />
+      </div> */}
+      {/* <div>
+        <h2>圆环内圈元素自定义</h2>
+        <DoughnutDemo1 />
+        <DoughnutDemo2 />
+      </div> */}
+      {/* <div>
+        <h2>自动轮播</h2>
+        <p>鼠标移上去会暂停，移开会继续</p>
+        <AutoDemo1 />
+        <AutoDemo2 />
+      </div> */}
+      <div>
+        <h2>双饼图</h2>
+        {/* <DoubleDemo1 /> */}
+        {/* <DoubleDemo2 /> */}
+        <DoubleDemo3 />
+      </div>
+
+      {/* <div>
+        <h2>label自定义</h2>
+        <LabelDemo1 />
+        <LabelDemo2 />
+        <LabelDemo3 />
+      </div> */}
+      {/* <div>
+        <h2>label自定义 + legend自定义</h2>
+        <LabelLegendDemo1 />
+        <LabelLegendDemo2 />
+      </div> */}
+    </React.Fragment>
   );
 }
---
+++ utils.js
@@ -0,0 +1,179 @@
+import defaultOption, { defaultColor } from './component/defaultOption';
+import ReactDOM from 'react-dom/client';
+
+export const formatterLegend = ({
+  option = {},
+  data = [],
+  color = defaultColor,
+  seriesIndexArr = [],
+}) => {
+  const legend = {
+    ...defaultOption.legend,
+    ...option,
+  };
+  const { content = '' } = option;
+  if (!content) return legend;
+
+  if (typeof content === 'function') {
+    const res = content({});
+
+    if (typeof res === 'string') {
+      if (seriesIndexArr.length) {
+        let totalData = flatAndUnique(data);
+        return {
+          ...legend,
+          formatter: (name) => {
+            if (!content) return name;
+            let params = {
+              name,
+            };
+            seriesIndexArr.forEach((seriesIndex) => {
+              const dataItem = data[seriesIndex] || [];
+              const dataItemTotal = dataItem.reduce(
+                (total, currentValue) => total + currentValue.value,
+                0
+              );
+
+              dataItem.forEach((item, index) => {
+                if (item.name === name) {
+                  const colorIndex =
+                    totalData.findIndex(
+                      (totalDataItem) => totalDataItem.name === name
+                    ) % color.length;
+
+                  const { value } = item;
+                  params = {
+                    ...params,
+                    value,
+                    dataIndex: index,
+                    color: color[colorIndex],
+                    percent: (value / dataItemTotal) * 100 || 0,
+                  };
+                }
+              });
+            });
+
+            if (typeof content === 'function') {
+              const res = content(params);
+
+              if (typeof res === 'string') {
+                return res;
+              }
+
+              return '';
+            }
+
+            return content;
+          },
+        };
+      } else {
+      }
+    } else {
+      return {
+        ...legend,
+        show: false,
+      };
+    }
+  }
+};
+
+export const flatAndUnique = (arr = []) => {
+  const map = new Map();
+  const loop = (array) => {
+    array.forEach((item) => {
+      if (Array.isArray(item)) {
+        loop(item);
+      } else if (!map.get(item)) {
+        map.set(item);
+      }
+    });
+  };
+
+  loop(arr);
+  return [...map.keys()];
+};
+
+export const formatterTooltip = (option = {}) => {
+  const { content = '' } = option;
+  const dom = document.createElement('div');
+  if (!content) {
+    return {
+      ...defaultOption.tooltip,
+      ...option,
+    };
+  }
+
+  return {
+    ...defaultOption.tooltip,
+    ...option,
+    formatter: (params, ticket, callback) => {
+      if (typeof content === 'function') {
+        const res = content(params);
+        if (typeof res === 'string') {
+          return content(params);
+        }
+        ReactDOM.createRoot(dom).render(res);
+        return dom;
+      }
+
+      return content;
+    },
+  };
+};
+
+export const formatterData = (data = [], seriesIndex = 0) => {
+  return data.map((item, index) => {
+    if (Array.isArray(item)) {
+      return formatterData(item, index);
+    }
+    return {
+      ...item,
+      show: true,
+      dataIndex: index,
+      seriesIndex,
+    };
+  });
+};
+
+export const getParams = ({
+  data = [],
+  index = 0,
+  color = defaultColor,
+  colorIndex,
+}) => {
+  const item = data[index] || {};
+
+  let totalBySeriersIndex = {};
+  let dataBySeriersIndex = {};
+  let dataIndex = index;
+
+  data.forEach((dataItem) => {
+    const { seriesIndex } = dataItem;
+    if (!totalBySeriersIndex[seriesIndex]) {
+      totalBySeriersIndex[seriesIndex] = 0;
+    }
+    if (!dataBySeriersIndex[seriesIndex]) {
+      dataBySeriersIndex[seriesIndex] = [];
+    }
+    if (dataItem.name === item.name) {
+      dataIndex = dataBySeriersIndex[seriesIndex].length;
+    }
+    dataBySeriersIndex[seriesIndex].push(dataItem);
+    totalBySeriersIndex[seriesIndex] += dataItem.value;
+  });
+
+  let _colorIndex = isNum(colorIndex) ? colorIndex : index;
+  if (_colorIndex > color.length) {
+    _colorIndex = _colorIndex % color.length;
+  }
+  const { value } = item;
+  return {
+    name: item.name,
+    value,
+    dataIndex,
+    color: color[_colorIndex],
+    percent: (value / totalBySeriersIndex[item.seriesIndex]) * 100 || 0,
+  };
+};
+
+export const isNum = (value) => typeof value === 'number';
