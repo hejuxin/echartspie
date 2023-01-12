bdce740c456fa5f0526f21d021be08710f947578
hejuxin <42193700+hejuxin@users.noreply.github.com>
2023-01-12 13:46:37 +0800

    readme

--- src/App.js
+++ src/App.js
@@ -27,36 +27,36 @@ export default function App() {
         <TooltipDemo1 />
         <TooltipDemo2 />
       </div>
-      {/* <div>
+      <div>
         <h2>label自定义</h2>
         <LabelDemo1 />
         <LabelDemo2 />
-      </div> */}
-      {/* <div>
+      </div>
+      <div>
         <h2>图例自定义</h2>
         <LegendDemo1 />
         <LegendDemo2 />
         <LegendDemo3 />
         <LegendDemo4 />
-      </div> */}
-      {/* <div>
+      </div>
+      <div>
         <h2>圆环内圈元素自定义</h2>
         <DoughnutDemo1 />
         <DoughnutDemo2 />
-      </div> */}
-      {/* <div>
+      </div>
+      <div>
         <h2>自动轮播</h2>
         <p>鼠标移上去会暂停，移开会继续</p>
         <AutoDemo1 />
         <AutoDemo2 />
         <AutoDemo3 />
-      </div> */}
-      {/* <div>
+      </div>
+      <div>
         <h2>双饼图</h2>
         <DoubleDemo1 />
         <DoubleDemo2 />
         <DoubleDemo3 />
-      </div> */}
+      </div>
     </React.Fragment>
   );
 }
