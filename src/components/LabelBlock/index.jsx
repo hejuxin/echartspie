6fe75df7e2205266d36d7bed97319f6be03c1332
hejuxin <42193700+hejuxin@users.noreply.github.com>
2023-02-06 18:17:19 +0800

    添加label内容与line的距离distanceToLabelLine，并对其他样式值进行计算

--- src/components/LabelBlock/index.jsx
+++ src/components/LabelBlock/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { getParams } from '../../utils';
+import { getParams, isNum } from '../../utils';
 import './index.css';
 
 const LabelBlock = (props) => {
-  const { data, labelPos, option, hightlightIndex, chartsWidth } = props;
+  const {
+    data,
+    labelPos,
+    option,
+    hightlightIndex,
+    chartsWidth,
+    distanceToLabelLine,
+  } = props;
   const label = option.label;
   const mode = label.mode || 'outsideLine';
 
@@ -29,8 +36,12 @@ const LabelBlock = (props) => {
         const startPos = posItem?.[0] || [];
         const endIndex = labelPos.length > 1 ? labelPos.length - 1 : 0;
         const endPos = posItem?.[endIndex] || [];
+        const startPosX = startPos?.[0];
         const endPosX = endPos?.[0];
-        const isLeft = startPos[0] - endPosX > 0;
+        let isLeft = true;
+        if (isNum(startPosX) && isNum(endPosX)) {
+          isLeft = startPosX - endPosX > 0;
+        }
 
         const isActive = hightlightIndex === index;
 
@@ -53,15 +64,18 @@ const LabelBlock = (props) => {
         let transformY = '-50%';
         let maxWidth = 'auto';
         let left = endPosX;
+
+        let distance = 0;
         if (mode === 'insideLine') {
+          distance = isNum(distanceToLabelLine) ? distanceToLabelLine : 0;
           if (isLeft) {
             textAlign = 'left';
-            transformX = 0;
+            transformX = `calc(${distance}px)`;
 
             left = endPosX > 0 ? endPosX : 0;
           } else {
             textAlign = 'right';
-            transformX = '-100%';
+            transformX = `calc(-100% + ${distance}px)`;
 
             left = endPosX > chartsWidth ? chartsWidth : endPosX;
           }
@@ -71,8 +85,10 @@ const LabelBlock = (props) => {
               ? endPos[0] - posItem[labelPos.length - 2]?.[0]
               : 0;
 
-          maxWidth = Math.abs(lineLength2);
+          // 先取正，再进行计算
+          maxWidth = Math.abs(lineLength2) - distance;
         } else if (mode === 'outsideLine') {
+          distance = isNum(distanceToLabelLine) ? distanceToLabelLine : 5;
           if (isLeft) {
             transformX = '-100%';
             maxWidth = endPosX;
@@ -80,6 +96,8 @@ const LabelBlock = (props) => {
             transformX = 0;
             maxWidth = chartsWidth - endPosX;
           }
+
+          maxWidth = maxWidth - distance;
         }
 
         return (
